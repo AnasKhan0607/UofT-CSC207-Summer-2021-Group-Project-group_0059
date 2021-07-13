dff3aa13ac44be3beaea54b2cc72c7773b0ffb72
Ahmad <ahmad.islah@mail.utoronto.ca>
2021-07-13 11:47:00 -0400

    Defined the User entity class and its subclasses

---
+++ phase1/src/RegularUser.java
@@ -0,0 +1,14 @@
+public class RegularUser extends User {
+    private String password;
+
+    public RegularUser(String username, String password) {
+        super(username);
+        this.password = password;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
