08a7c5891270288915ce66744c8d0be7b98bd6b0
prl12 <ruilin.peng@mail.utoronto.ca>
2021-07-15 03:41:36 -0400

    7/15 User,Usermanager initialized

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -1,12 +1,7 @@
-public abstract class User {
+public class User {
     private String username;
-    public User (String username) {
-        this.username = username;
-    }
-    public String getUsername() {
-        return this.username;
-    }
-    public void setUsername (String username) {
+
+    public User(String username){
         this.username = username;
     }
 }
---
+++ phase1/src/UserManager.java
@@ -0,0 +1,15 @@
+public class UserManager {
+    private String[] bufferedUsers;
+
+    public UserManager(){}
+
+    public void addUser(){}
+
+    public User SearchUser(String username){
+
+    }
+
+
+}
+
+
