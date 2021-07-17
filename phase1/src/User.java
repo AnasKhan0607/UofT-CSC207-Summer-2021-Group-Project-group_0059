997006bc8a00c1e7eea1c2f609e90d4f3c312114
prl12 <ruilin.peng@mail.utoronto.ca>
2021-07-17 13:36:03 -0400

    7/17 User set method

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -8,5 +8,9 @@ public class User {
     public String getUsername() {
         return username;
     }
+
+    public void setUsername (String username) {
+        this.username = username;
+    }
 }
 
