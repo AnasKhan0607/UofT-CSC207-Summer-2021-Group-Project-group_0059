f98dba78523184b89ec47d2f0287d43623c65900
Kimiya Raminrad <k.raminrad@mail.utoronto.ca>
2021-07-19 12:19:39 -0400

    added to Game.java

--- phase1/src/Game.java
+++ phase1/src/Game.java
@@ -1,6 +1,7 @@
+
 import java.util.HashMap;
 
-public class Game{
+public class Game {
     class GameNode{
         private String nodeID;
         private String dialogue;
@@ -8,22 +9,33 @@ public class Game{
 
     }
 
-    private String gameID;
+    private static int gameID;
+    //1st digit is template number, for example, 1 for template 1
     private String gameName;
     private String gameAuthor;
+    private GameNode firstNode;
     private String gameState;
     private int choicesNum;
 
-    public Game(String gameName, String gameAuthor, String gameState, int choicesNum) {
+    public Game(String gameName, String gameAuthor, String gameState, int choicesNum, GameNode firstNode) {
         this.gameName = gameName;
         this.gameAuthor = gameAuthor;
         this.gameState = gameState;
         this.choicesNum = choicesNum;
+        this.firstNode = firstNode;
     }
 
+    public int GameIdGetters(){return gameID;}
+    public String GameNameGetters(){return gameName;}
+    public String AuthorGetters(){return gameAuthor;}
+    public GameNode FirstNodeGetters(){return firstNode;}
+    public String GameStateGetters(){return gameState;}
+    public int ChoiceNumGetters(){return choicesNum;}
+
+    public void GameIdSetters(int givenGameId){
+        gameID = givenGameId;}
     private String generateID(){
         return "123";
     }
-
 
 }
