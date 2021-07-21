1b7df38f000ec5b3889bf2243182b8a4909f9786
BuildTools <unconfigured@null.spigotmc.org>
2021-07-21 00:49:21 -0400

    Reading data from csvs

--- phase1/src/GameGate.java
+++ phase1/src/GameGate.java
@@ -1,4 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class GameGate {
+
+    String myPath = new File("").getAbsolutePath();
+
+    public Game load_game() {
+
+
+
+    }
+
+
 
 }
 
