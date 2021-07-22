d607992b7968a4f767888e8689e03bd76c28efcc
BuildTools <unconfigured@null.spigotmc.org>
2021-07-22 01:04:15 -0400

    working on Array to csv

---
+++ phase1/src/TableToCsv.java
@@ -0,0 +1,7 @@
+public class TableToCsv {
+//      From StackOverflow:
+//    List<String> objects= Arrays.asList("Test1","Test2","Test3");
+//    String objectsCommaSeparated = String.join(",", objects);
+//    System.out.println(objectsCommaSeparated);
+//
+}
