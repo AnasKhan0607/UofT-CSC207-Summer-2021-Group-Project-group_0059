1a3aac14e4f5d8ac858cdee0ca5284acd58e6eee
BuildTools <unconfigured@null.spigotmc.org>
2021-07-22 19:25:56 -0400

    writing general classes to read and write arrays

--- phase1/src/TableToCsv.java
+++ phase1/src/TableToCsv.java
@@ -1,7 +1,31 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class TableToCsv {
-//      From StackOverflow:
-//    List<String> objects= Arrays.asList("Test1","Test2","Test3");
-//    String objectsCommaSeparated = String.join(",", objects);
-//    System.out.println(objectsCommaSeparated);
-//
+//      From StackOverflow: https://stackoverflow.com/questions/35749250/java-write-the-list-string-into-csv-file
+
+    public TableToCsv() throws IOException {
+
+
+        FileWriter writer = new FileWriter(new File("").getAbsolutePath() + "\\phase1\\data\\WriteData.csv");
+
+        String input = "[\"user1\",\"track1\",\"player1\", \"user1\",\"track2\",\"player2\", \"user1\",\"track3\",\"player3\"]";
+        input = input.substring(1, input.length() - 1); // get rid of brackets
+        String[] split = input.split(" ");
+
+        for(String s : split) {
+            String[] split2 = s.split(",");
+            writer.write(Arrays.asList(split2).stream().collect(Collectors.joining(",")));
+            writer.write("\n"); // newline
+        }
+
+        writer.close();
+
+
+    }
+
 }
