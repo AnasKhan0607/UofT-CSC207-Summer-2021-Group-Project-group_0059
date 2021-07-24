a11a390cdc8625dacc13c4738711d0e14c6d9998
prl12 <ruilin.peng@mail.utoronto.ca>
2021-07-24 13:01:36 -0400

    UserLoginPresenter updated

--- phase1/Controller/UserLoginController.java
+++ phase1/Controller/UserLoginController.java
@@ -14,33 +14,38 @@ public class UserLoginController {
 
     public void NormalUserinput(){
         Scanner myObj = new Scanner(System.in);
-        /*UserLoginPresenter.displayUsername();*/
+        UserLoginPresenter.display();
         this.userName = myObj.nextLine();
-        this.password = myObj.nextLine();
-        if (testUM.SearchUser(this.userName) == null){
-            System.out.println("Username does not exist");
+        if (this.userName == "Signup"){
+            /* Jump to signup */
+        } else if (this.userName == "Guest"){
+            GuestUserInput();
         } else {
-            String temppassword = testUM.SearchUser(this.userName).getPassword();
-            if (!temppassword.equals(this.password)){
-                System.out.println("Username does not exist");
+            this.password = myObj.nextLine();
+            if (testUM.SearchUser(this.userName) == null){
+                UserLoginPresenter.errorMessage();
             } else {
-                System.out.println("Login successful!");
-                System.out.println("Redirecting...");
-            }
+                String temppassword = testUM.SearchUser(this.userName).getPassword();
+                if (!temppassword.equals(this.password)){
+                    UserLoginPresenter.errorMessage();
+                } else {
+                    UserLoginPresenter.successMessage();
+                }
 
+            }
         }
+
     }
 
     public void GuestUserInput(){
         Scanner myObj = new Scanner(System.in);
         ArrayList<String> info = new ArrayList<>();
         /*UserLoginPresenter.displayUsername();*/
-        System.out.println("input username. press enter to continue");
+        UserLoginPresenter.display2();
         this.userName = myObj.nextLine();
         info.add(this.userName);
         testUM.addUser(info);
-        System.out.println("Logged in as Guest");
-        System.out.println("Redirecting...");
+        UserLoginPresenter.successMessage();
     }
 
 
--- phase1/Presenter/UserLoginPresenter.java
+++ phase1/Presenter/UserLoginPresenter.java
@@ -1,9 +1,22 @@
 public class UserLoginPresenter {
 
-    public void display(){
+    public static void display(){
         System.out.println("Please login first:");
-        System.out.print("Please input your username first, press enter and then" +
-                "your password, and press enter again to continue");
+        System.out.println("Please input your username first, press enter and then" +
+                "your password, and press enter again to continue.");
+        System.out.println("Don't have an account? Type Signup to register or Guest to login as a guest")
 
+    }
+
+    public static void display2(){
+        System.out.println("input username. press enter to continue");
+    }
+
+
+    public static void errorMessage(){System.out.println("Sorry, but either username or password is incorrect")}
+
+    public static void successMessage(){
+        System.out.println("Logged in as Guest");
+        System.out.println("Redirecting...");
     }
 }
