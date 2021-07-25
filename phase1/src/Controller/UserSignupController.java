422c0bd314077ed5c196bfdf332b16c72e9c4643
Ahmad <ahmad.islah@mail.utoronto.ca>
2021-07-24 20:41:34 -0400

    UserSignupController class is almost done, may still alter it

--- phase1/src/Controller/UserSignupController.java
+++ phase1/src/Controller/UserSignupController.java
@@ -1,9 +1,35 @@
 package Controller;
 
+import Presenter.UserSignUpPresenter;
+import UseCase.UserManager;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class UserSignupController {
     private ArrayList<String> userInput;
+    private String username;
+    private String password;
+    private UserManager testUM = new UserManager();
 
-//    public
+    public UserSignupController() {
+        this.username = null;
+        this.password = null;
+        this.userInput = new ArrayList<>();
+    }
+
+    public void UserInput() {
+        Scanner myObj = new Scanner(System.in);
+        UserSignUpPresenter.display();
+        this.username = myObj.nextLine();
+        this.password = myObj.nextLine();
+        while (testUM.SearchUser(this.username) != null) {
+            UserSignUpPresenter.errorMessage();
+            this.username = myObj.nextLine();
+        }
+        userInput.add(this.username);
+        userInput.add(this.password);
+        testUM.addUser(userInput);
+        UserSignUpPresenter.successMessage();
+    }
 }
