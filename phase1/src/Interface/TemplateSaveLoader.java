8faa609c8235dbc30689ceccadefeeca99e07daa
tiantan4 <tian.tan@mail.utoronto.ca>
2021-07-26 16:18:19 -0600

    templategate and interface

--- phase1/src/Interface/TemplateSaveLoader.java
+++ phase1/src/Interface/TemplateSaveLoader.java
@@ -5,5 +5,5 @@ import java.util.List;
 
 public interface TemplateSaveLoader {
     List<HashMap> load_templates();
-    void save_templates(List<HashMap> gameTable);
+    void save_templates(List<HashMap> TemplateTable);
 }
