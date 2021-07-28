77f4e6497b2cfd9e9d7ad6e115c301bfc7cfbef2
Daniel Liu <danielzf.liu@mail.utoronto.ca>
2021-07-27 22:52:26 -0400

    Updated GamePresenter.java and ArtGallery.java

--- phase1/src/Presenter/GamePresenter.java
+++ phase1/src/Presenter/GamePresenter.java
@@ -1,7 +1,26 @@
 package Presenter;
+import Presenter.ArtGallery;
 
 public class GamePresenter {
-    public void playGame(){
+    public static void main(String[] args) {
+        System.out.println(ArtGallery.menuArt);
+    }
+    public void menu(){
+
+    }
+    public void createGame(String templateChoice){
+
+    }
+    public void playGame(String gameName){
         System.out.println("");
+    }
+    public void displayPublicGames() {
+
+    }
+    public void displayPrivateGames(String myName){
+
+    }
+    public void displayGamesByAuthor(String authorName){
+
     }
 }
