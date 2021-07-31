c83cf80d8cd2c662585b726dd1ea15eafb768a75
Daniel Liu <danielzf.liu@mail.utoronto.ca>
2021-07-30 20:44:59 -0400

    uml for game branch added

--- phase1/src/Exception/AddDialogueException.java
+++ phase1/src/Exception/AddDialogueException.java
@@ -1,10 +1,6 @@
 package Exception;
 
 public class AddDialogueException extends Exception{
-    public static void main(String[] args) {
-        Exception exception = new AddDialogueException("bruh", new IndexOutOfBoundsException());
-//        System.out.println(exception.getCause());
-    }
     public AddDialogueException() { super(); }
     public AddDialogueException(String message) { super(message); }
     public AddDialogueException(String message, Throwable cause) { super(message, cause); }
