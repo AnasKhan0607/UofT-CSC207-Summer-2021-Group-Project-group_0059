c3724c7d030dd787da8bc0e75681fe0447c0041c
BuildTools <unconfigured@null.spigotmc.org>
2021-08-15 21:13:23 -0400

    Change name to be more appropriate.

--- phase2/src/Gateway/GameGateAWS.java
+++ phase2/src/Gateway/GameGateAWS.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class GameGateFirebase {
+public class GameGateAWS {
     public static void main(String[] args) {
         String  url = "jdbc:mysql://dbphase2.cy2xtdsstzct.us-east-2.rds.amazonaws.com:3306/";
         String username = "admin";
@@ -12,6 +12,7 @@ public class GameGateFirebase {
 
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to Database!");
 
         } catch (SQLException e) {
             System.out.println("Error connecting!");
