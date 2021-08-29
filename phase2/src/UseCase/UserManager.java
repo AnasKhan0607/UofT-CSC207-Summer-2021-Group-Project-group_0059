4c6f0709b8e5da6c952a96282fac1353d84e1b09
Ahmad <ahmad.islah@mail.utoronto.ca>
2021-08-29 15:01:08 -0400

    Added to the javadocs of the Use Case classes

--- phase2/src/UseCase/UserManager.java
+++ phase2/src/UseCase/UserManager.java
@@ -26,6 +26,7 @@ public class UserManager {
 
     /**
      * Creates a list of users using the UserGate class. and for every suspended user, checks his/her suspension end date that if it's today and decide whether to unsuspend automatically
+     * @param gate the object (its type is the LoadSave interface) used to access the UserGate methods
      */
     public UserManager(LoadSave gate){
         this.bufferedUsers = new ArrayList<>();
